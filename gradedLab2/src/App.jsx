51971248bbdd506e7b167007802f070975191fa3
Tshepiso-333 <tshepisomolefe369@gmail.com>
2025-08-04 21:48:56 +0200

    changes

--- gradedLab2/src/App.jsx
+++ gradedLab2/src/App.jsx
@@ -3,6 +3,9 @@ import ProfileCard from "./components/ProfileCard";
 import './App.css';
 import gojoImg from './assets/gojo.jpeg'
 import yutaImg from './assets/yuta.jpeg'
+import tojiImg from './assets/toji.jpeg'
+import kisukeImg from './assets/kisuke.jpeg'
+import killuaImg from './assets/killua.jpeg'
 import yujiImg from './assets/yuji.jpeg'
 
 
@@ -29,6 +32,28 @@ function App() {
           name="Yuji Itadori"
           title="A Grade socerer"
           image={yujiImg}
+          bio="Passionate about creating user-friendly interfaces and modern web experiences. Contact: sarah@company.com"
+        />
+
+        <ProfileCard 
+          name="Toji fushiguro"
+          title=" Sorcerer Killer"
+          image={tojiImg}
+          bio="Cant use curse energy but kills sorcerer for a living"
+        />
+
+        <ProfileCard 
+          name="Killua"
+          title=" aura farmer"
+          image={killuaImg}
+          bio="Passionate about creating user-friendly interfaces and modern web experiences. Contact: sarah@company.com"
+        />
+
+
+        <ProfileCard 
+          name="kisuke urahara"
+          title=" Heavenly Restrictions"
+          image={kisukeImg}
           bio="Passionate about creating user-friendly interfaces and modern web experiences. Contact: sarah@company.com"
         />
       </div>
